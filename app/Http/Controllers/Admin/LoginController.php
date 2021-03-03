a126fd8b6d71ed851a9a5e4be42cf96e2cff02b1
奔走的少年 <229235151@qq.com>
2021-03-03 18:44:39 +0800

    Laravlel博客

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Model\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\View;
+
+require_once 'resources/org/code/Code.class.php';
+
+class LoginController extends CommonController
+{
+    public function login()
+    {
+        if($input = Input::all()){
+            $code = new \Code;
+            $_code = $code->get();
+            if(strtoupper($input['code'])!=$_code){
+                return back()->with('msg','验证码错误！');
+            }
+            $user = User::first();
+            if($user->user_name != $input['user_name'] || Crypt::decrypt($user->user_pass)!= $input['user_pass']){
+                return back()->with('msg','用户名或者密码错误！');
+            }
+            session(['user'=>$user]);
+            return redirect('admin');
+
+        }else {
+            return view('admin.login');
+        }
+    }
+
+    public function quit()
+    {
+        session(['user'=>null]);
+        return redirect('admin/login');
+    }
+
+    //密码加密
+//    public function crypt(){
+//        $str = '123456';
+//       $str_p = Crypt::encrypt($str);
+//        echo $str_p.'<br/>';
+//        echo Crypt::decrypt($str_p);
+//    }
+
+    public function code()
+    {
+        $code = new \Code;
+        $code->make();
+    }
+
+}
